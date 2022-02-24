87b268e58a0c7224f3909c8c59c7610755d5855f
Juaneth <euanwalker2008@gmail.com>
2022-02-24 20:51:43 +0000

    bedder

--- notes/server.md
+++ notes/server.md
@@ -1,14 +1,3 @@
 # Server
 
 The server is based on Express and is an API to handle requests from the Argon Client. To keep synced we're gonna run multiple instances of this on multiple servers then sync every 10 minutes, once we get 2500 daily users we will expand to a VPS
-
-
----- Meg's Node.js love ----
-node.js is for losers, even tho i make all my stuff in it, its very shit and i hate it will all my heart and wish it would burn in a house fire, you get me?, no then fuck you, you should also die in a fucking fire.
-
-
----- Meg's hate for his kitten ----
-eat it all asians i give you all right to eat it for luch, dinner and the else idk, kill it any way you can
-
----- Meg's hate for french people ----
-French people, why do they exist, i hate all of them, the only good thing they made was fast vrooom car, stil shit tho, and semi circle bread thing, WARZONE FRECH PEOPLE I HATE THEM THE MOST, bonjor my cock you bread stick mucher cunt, get good you cunt burnt the france would be better if hittler kept it and it would be good, guten tag Adolf, we love you, and we hate the french, burn the french, molest the french, burn the french on a spoke, hang them on a pillar, steal there semi circle bread things and their fast cars, kill the french steal their big pointy tower, steal all items the french own, burn them, give me there soul, let me commence a curse opoin them and ruin there lives 1 by 1, murder them all 2 by 4, all arson or crimes agesnt french should be legal, but oh no the fags come along, "all lives matter" no they fucking dont, well not the frenchs at least. 
--- server/setup.js
+++ server/setup.js
@@ -1,52 +1,29 @@
 const fs = require('fs')
 
-console.log('By running this script you agree to our terms of use defined here: https://github.com/juaneth/Argon and once script is run all userdata will be cleared if there were previously. \n\n')
+console.log('By running this script you agree to our terms of use defined here: https://github.com/juaneth/Argon and once script is run all data will be cleared if there were previously. \n\n')
 
 const prompts = require('prompts');
 
 (async() => {
-    const response = await prompts({
-        type: 'text',
-        name: 'value',
-        message: 'Y/N?',
-    });
-
-    if (response.value == 'y' || response.value == 'Y' || response.value == 'yes' || response.value == 'YES') {
-        const { exec } = require("child_process");
-
-        let child = exec(``, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-
-            console.log(`stdout: ${stdout}`);
+        const response = await prompts({
+            type: 'text',
+            name: 'value',
+            message: 'Y/N?',
         });
 
-        // Create logins folder for login info
-        try {
-            fs.mkdirSync('userdata/logins')
-        } catch {
-            console.log('./userdata and ./userdata/logins both already exist - Skipping')
-        }
+        if (response.value == 'y' || response.value == 'Y' || response.value == 'yes' || response.value == 'YES') {
+            const { exec } = require("child_process");
 
-        // Write users.json to store user info
-        try {
-            fs.writeFileSync('userdata/users.json', '{}')
-        } catch {
-            console.log('./userdata/users.json already exists - Skipping')
-        }
+            let child = exec(`npm i`, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
 
-        try {
-            fs.writeFileSync('userdata/invites.json', '{}')
-        } catch {
-            console.log('./userdata/invites.json already exists - Skipping')
-        }
-    } else {
-        console.log('\n\nExiting...')
-    }
-})();
+                console.log(`stdout: ${stdout}`);
+            });
+        })();
