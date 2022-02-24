9954232c0ffac7f2a754d80c697beebad688f826
Juaneth <euanwalker2008@gmail.com>
2022-02-24 21:22:22 +0000

    yeaha

--- server/index.js
+++ server/index.js
@@ -9,41 +9,35 @@ function log(content) {
     console.log(content)
 
     let prev = fs.readFileSync('log.txt', 'utf-8')
-    fs.writeFileSync()
+    fs.writeFileSync('log.txt', `${prev}\n${content}`)
 }
 
 function database(action, content) {
-    function save(where, what) {
-        fs.writeFile("./userdata/invites.json", jsonData, function(err) {
-            if (err) {
-                console.log(err);
-            }
-        });
-    }
-
-    //Save User to Database
+    // Save User to Database
     if (action == undefined || content == undefined) {
         log('Undefined action or content for Database')
     } else {
         if (action == "create-user") {
-
+            // Create user in database, eg. save their username and generate a 64 bit key
         }
 
         if (action == "login") {
-
+            // Find their key in database and verify them eg. read key and decrypt it and then check.
         }
 
         if (action == "invite") {
-
+            // Generate their invite and minus 1 to the account generating the invite
         }
     }
 }
+
+// API Setup
 
 const app = express()
 
 const port = 1455
 
-//generate userid with specified username
+// Generate userid using uuid package and return it
 function generateuserid() {
     const { v4: uuidv4 } = require('uuid');
 
@@ -55,11 +49,13 @@ function generateuserid() {
 app.get('/create-acc', (req, res) => {
     let username = req.query.username
 
-    //generate a user id for account
+    // Generate user id
     const useridgenerated = generateuserid();
 
+    // Create user in database
     database("create-user", username)
 
+    // Respond with success
     res.json({
         "success": "Account was created for user",
         "username": `@${username}`,
@@ -70,9 +66,9 @@ app.get('/create-acc', (req, res) => {
 app.get('/create-inv', (req, res) => {
     let username = req.query.username
 
-    //Generate passkey
+    // Generate invite key
     let passkey = password.generate({
-        length: 12,
+        length: 24,
         numbers: false
     });
 
@@ -85,20 +81,21 @@ app.get('/create-inv', (req, res) => {
 
 app.get('/login', (req, res) => {
     let username = req.query.username
+    let key = req.query.key
 
-    //Generate passkey
-    let passkey = password.generate({
-        length: 12,
-        numbers: false
+    let newkey = password.generate({
+        length: 128,
+        numbers: true,
     });
 
+    database('login', `${username}--${key}`)
+
     res.json({
-        "success": "Invite was created for user",
-        "author": `@${username}`,
-        "invite-key": `inv-${passkey}`
+        "success": "Logged in",
+        "username": `@${username}`,
+        "new-key": `${newkey}`
     })
 });
 
 app.listen(port, () =>
-    console.log(`Argon API listening on port ${port}! TEST: http://localhost:${port}/create-inv?username=TEST`),
-);
+    console.log(`Argon API listening on port ${port}! TEST: http://localhost:${port}/create-inv?username=TEST`), );
--- server/setup.js
+++ server/setup.js
@@ -1,29 +1,32 @@
 const fs = require('fs')
 
-console.log('By running this script you agree to our terms of use defined here: https://github.com/juaneth/Argon and once script is run all data will be cleared if there were previously. \n\n')
+console.log('By running this script you agree to our terms of use defined here: https://github.com/juaneth/Argon and once script is run all userdata will be cleared if there were previously. \n\n')
 
 const prompts = require('prompts');
 
 (async() => {
-        const response = await prompts({
-            type: 'text',
-            name: 'value',
-            message: 'Y/N?',
+    const response = await prompts({
+        type: 'text',
+        name: 'value',
+        message: 'Y/N?',
+    });
+
+    if (response.value == 'y' || response.value == 'Y' || response.value == 'yes' || response.value == 'YES') {
+        const { exec } = require("child_process");
+
+        let child = exec(`npm i`, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+
+            console.log(`stdout: ${stdout}`);
         });
-
-        if (response.value == 'y' || response.value == 'Y' || response.value == 'yes' || response.value == 'YES') {
-            const { exec } = require("child_process");
-
-            let child = exec(`npm i`, (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-
-                console.log(`stdout: ${stdout}`);
-            });
-        })();
+    } else {
+        console.log('\n\nExiting...')
+    }
+})();
